5bc93c03c4ed303d764afd222f1c2e497ce72e25
Tadeo Kondrak <me@tadeo.ca>
2019-05-15 19:52:29 -0600

    add track::read_styles

--- libass/src/track.rs
+++ libass/src/track.rs
@@ -1,3 +1,5 @@
+use std::ffi::CStr;
+
 pub struct Track<'library>(&'library mut libass_sys::ass_track);
 
 impl<'library> Track<'library> {
@@ -9,12 +11,22 @@ impl<'library> Track<'library> {
         self.0
     }
 
+    pub fn step_sub(&self, now: i64, movement: i32) -> i64 {
+        unsafe { libass_sys::ass_step_sub(self.0 as *const _ as *mut _, now, movement) }
+    }
+
     pub fn process_force_style(&mut self) {
         unsafe { libass_sys::ass_process_force_style(self.0) }
     }
 
-    pub fn step_sub(&self, now: i64, movement: i32) -> i64 {
-        unsafe { libass_sys::ass_step_sub(self.0 as *const _ as *mut _, now, movement) }
+    pub fn read_styles(&mut self, filename: &CStr, codepage: &CStr) {
+        unsafe {
+            libass_sys::ass_read_styles(
+                self.0,
+                filename.as_ptr() as *mut _,
+                codepage.as_ptr() as *mut _,
+            );
+        }
     }
 }
 
